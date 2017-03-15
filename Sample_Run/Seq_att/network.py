32e5fa18ace4841d56b4f72d4f01a8a6c1e8ab34
Yash Chandak <yashchandak@yahoo.in>
2017-03-15 22:44:36 +0530

    Better Attention module

--- Sample_Run/Seq_att/network.py
+++ Sample_Run/Seq_att/network.py
@@ -156,19 +156,46 @@ class Network(object):
 
         return rnn_outputs
 
-    def attention(self, features, context):
+    def attention2(self, features, context):
         feat_cat = tf.pack([tf.concat(1, [features[tstep], context]) for tstep in range(len(features))])
         features = tf.pack(features)
 
         with tf.variable_scope('Attention') as scope:
-            l,b,f = feat_cat.get_shape().as_list()
-            W_attn = tf.get_variable('W_attn', [1,1,f])
+            l, b, f = feat_cat.get_shape().as_list()
+            W_attn = tf.get_variable('W_attn', [1, 1, f])
             vals = tf.reduce_sum(tf.mul(feat_cat, W_attn), axis=2, keep_dims=True)
-            alpha = tf.nn.softmax(tf.nn.tanh(vals), dim=0) #softmax across the time-step dimension
-            c = tf.reduce_sum(alpha*features, axis=0) #weighted sum across the time-step dimension
+            alpha = tf.nn.softmax(tf.nn.tanh(vals), dim=0)  # softmax across the time-step dimension
+            c = tf.reduce_sum(alpha * features, axis=0)  # weighted sum across the time-step dimension
             scope.reuse_variables()
 
         return c
+
+    def attention(self, states, context, attn_size=None):
+        states = tf.pack(states) #convert from list to tensor
+
+        num_step, batch_size, state_size = states.get_shape().as_list()
+        if attn_size == None:  # size of the intermediate attention representation
+            attn_size = state_size  # by default A = state_size
+        attn_length = num_step #length of attention vector = Num_step
+
+        hidden = tf.reshape(states,[-1, attn_length, 1, state_size]) # [Batch, Num_step, 1, state_size]
+        # filter
+        k = tf.get_variable("AttnW",[1, 1, state_size, attn_size]) # [1, 1, state_size, A]
+        attn_features = tf.nn.conv2d(hidden, k, [1,1,1,1], "VALID") # [Batch, Num_step, 1, state_size] * [1, 1, state_size, A] = [Batch, Num_step, 1, A]
+        attention_softmax_weights = tf.get_variable("W_attention_softmax", [attn_size]) # [A]
+
+        y = tf.nn.rnn_cell._linear(args = context, output_size = attn_size, bias = True) # W*C + b : [Batch, context_size] -> [Batch, A]
+        y = tf.reshape(y, [-1, 1, 1, attn_size])  # Reshape into 4D: [Batch, 1, 1, A]
+
+        # Calculating alpha
+        s = tf.reduce_sum(attention_softmax_weights * tf.nn.tanh(attn_features + y), [2, 3]) # [A]*[Batch, Num_step, 1, A] -> [Batch, Num_step]
+        a = tf.nn.softmax(s) # [Batch, Num_step]
+
+        # Calculate context c
+        c = tf.reduce_sum(tf.reshape(a, [-1, attn_length, 1, 1]) * hidden, [1, 2]) #[Batch, Num_step, 1, 1]* [Batch, Num_step, 1, state_size] -> [Batch, state_size]
+
+        return c
+
 
 
     def loss(self, predictions, labels):
